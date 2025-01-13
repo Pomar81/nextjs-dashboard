f84f602becbf3f664f8c496a155ee20a96c7f327
spomaraiko <spomaraiko@exadel.com>
2025-01-13 19:22:34 +0200

    lesson 13

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -7,7 +7,7 @@ import {
   LatestInvoiceRaw,
   Revenue,
 } from "./definitions";
-import { formatCurrency } from "./utils";
+import { formatCurrency } from "@/app/lib/utils";
 
 export async function fetchRevenue() {
   try {
